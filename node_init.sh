5241e81781706ccb91b7749efb21bd270056b45f
ahmeds26 <ahmed.saeeed@outlook.com>
2023-01-28 18:35:20 +0200

    Add files to repo

--- node_init.sh
+++
@@ -1,7 +0,0 @@
-git init
-npx license "$(npm get init.license)" -o "$(npm get init.author.name)"
-npx gitignore node
-npx covgen "$(npm get init.author.email)"
-npm init -y
-git add -A
-git commit -m "Initial commit"
